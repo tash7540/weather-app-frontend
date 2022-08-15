6fc4ff50462664358327b0db1529b49fe3d3e0b6
tash7540 <59746687+tash7540@users.noreply.github.com>
2022-08-15 08:20:31 -0600

    readded mock data

--- src/components/Chart/chart.js
+++ src/components/Chart/chart.js
@@ -4,22 +4,26 @@ import {useSelector } from 'react-redux';
 
 const Chart = ()=>{
   const timelinedata = useSelector((state) => state.timelineData);
-  //timelinedata.weatherData.map((data) =>{
-  //  console.log(data);
-  //});
-  //console.log(timelinedata.weatherData[50]);
+  console.log('a'+timelinedata.weatherData);
   return(
     <Card>
       <LineChart
         axisLabels={{x: 'Time', y: 'Temp'}}
         margin={{top: 10, right: 30, bottom: 50, left: 70}}
-        xType={'text'}
+        xType={'time'}
         axes
         interpolate={'cardinal'}
         width={650}
         height={270}
         data={[
-          timelinedata.weatherData
+          //timelinedata.weatherData
+          [
+            { x: '1-Jan-15', y: 20 },
+            { x: '1-Feb-15', y: 10 },
+            { x: '1-Mar-15', y: 33 },
+            { x: '1-Apr-15', y: 45 },
+            { x: '1-May-15', y: 15 }
+          ]
         ]
         }
       />
